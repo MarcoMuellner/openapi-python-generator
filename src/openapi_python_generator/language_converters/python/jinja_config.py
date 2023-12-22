92b3ce1f98910441ede4308ab9c9acf3a2c8304d
Stuart Fisher <stuart.fisher@esrf.fr>
2023-12-22 08:57:27 +0100

    basic template customisation

--- src/openapi_python_generator/language_converters/python/jinja_config.py
+++ src/openapi_python_generator/language_converters/python/jinja_config.py
@@ -1,7 +1,8 @@
+import os
 from pathlib import Path
 
 from jinja2 import Environment
-from jinja2 import FileSystemLoader
+from jinja2 import FileSystemLoader, ChoiceLoader
 
 
 ENUM_TEMPLATE = "enum.jinja2"
@@ -12,5 +13,16 @@ API_CONFIG_TEMPLATE = "apiconfig.jinja2"
 TEMPLATE_PATH = Path(__file__).parent / "templates"
 
 JINJA_ENV = Environment(
-    loader=FileSystemLoader(TEMPLATE_PATH), autoescape=True, trim_blocks=True
+    loader=(
+        ChoiceLoader(
+            [
+                FileSystemLoader(os.environ["CUSTOM_TEMPLATE_PATH"]),
+                FileSystemLoader(TEMPLATE_PATH),
+            ]
+        )
+        if os.environ.get("CUSTOM_TEMPLATE_PATH")
+        else FileSystemLoader(TEMPLATE_PATH)
+    ),
+    autoescape=True,
+    trim_blocks=True,
 )
